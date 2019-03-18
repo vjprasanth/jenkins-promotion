cab72ec1e77fabc0736e0f390ec128b7d68cebc0
Prasanth Jayapraksh <pjayaprakash@equalexperts.com>
2019-03-18 11:22:39 +0200

    upgrade the webpack version to 4 and move away from the default vue template application to a simple vue app.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # jenkins-promotion
 An example project to show how to use promotion plugin along with Jenkins pipeline and job dsl.
+
+Sample Vue project has been created based on this article.
+https://itnext.io/getting-started-with-vue-webpack-4-babel-edd3e577421e
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,27 @@
-var path = require('path');
-var webpack = require('webpack');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/main.js',
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        publicPath: '/dist/',
-        filename: 'build.js'
-    },
-    resolveLoader: {
-        root: path.join(__dirname, 'node_modules'),
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.vue$/,
-                loader: 'vue'
-            },
-            {
-                test: /\.js$/,
-                loader: 'babel',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.(png|jpg|gif|svg)$/,
-                loader: 'file',
-                query: {
-                    name: '[name].[ext]?[hash]'
-                }
-            }
-        ]
-    },
-    devServer: {
-        historyApiFallback: true,
-        noInfo: true
-    },
-    devtool: '#eval-source-map'
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        exclude: /node_modules/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
 };
-
-if (process.env.NODE_ENV === 'production') {
-    module.exports.devtool = '#source-map';
-    // http://vue-loader.vuejs.org/en/workflow/production.html
-    module.exports.plugins = (module.exports.plugins || []).concat([
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: '"production"'
-            }
-        }),
-        new config.optimization.splitChunks({
-            compress: {
-                warnings: false
-            }
-        })
-    ])
-}
